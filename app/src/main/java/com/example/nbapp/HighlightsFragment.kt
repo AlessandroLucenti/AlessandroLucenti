650a89748058330f82addf95a95cfcd3553c9294
AlessandroLucenti <alessandro.lucenti@edu.itspiemonte.it>
2022-06-21 13:50:36 +0200

    bestemmie gratis

--- app/src/main/java/com/example/nbapp/HighlightsFragment.kt
+++ app/src/main/java/com/example/nbapp/HighlightsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +18,10 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HighlightsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
+
+
 class HighlightsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -24,16 +30,16 @@ class HighlightsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+            }
+
         }
     }
 
-    override fun onCreateView(
+    fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_highlights, container, false)
     }
-}
